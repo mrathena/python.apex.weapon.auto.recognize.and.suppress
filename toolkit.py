293a30b8b3d573cafe34a85a9a23eb3721a24420
mrathena <nobody@mrathena.com>
2022-09-20 22:10:50 +0800

    .

--- toolkit.py
+++ toolkit.py
@@ -323,4 +323,3 @@ class Game:
         data[cfg.turbo] = Game.turbo(bullet, arms)
         t2 = time.perf_counter_ns()
         print(f'耗时:{t2-t1}ns, 约{(t2-t1)//1000000}ms, {gun.get(cfg.name)}{"(涡轮)" if data.get(cfg.turbo) else ""}')
-
