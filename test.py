0082f10a1f1b259bae2e10097a721422023e3707
mrathena <nobody@mrathena.com>
2022-09-28 12:01:15 +0800

    .

--- test.py
+++ test.py
@@ -1,64 +1,40 @@
 import ctypes
+import time
 from ctypes import wintypes
 
 import win32con
 import win32gui
 
-class BITMAPINFOHEADER(ctypes.Structure):
-    _fields_ = [
-        ("biSize", wintypes.DWORD),
-        ("biWidth", ctypes.c_long),
-        ("biHeight", ctypes.c_long),
-        ("biPlanes", wintypes.WORD),
-        ("biBitCount", wintypes.WORD),
-        ("biCompression", wintypes.DWORD),
-        ("biSizeImage", wintypes.DWORD),
-        ("biXPelsPerMeter", ctypes.c_long),
-        ("biYPelsPerMeter", ctypes.c_long),
-        ("biClrUsed", wintypes.DWORD),
-        ("biClrImportant", wintypes.DWORD)
-    ]
-
-
-class BITMAPINFO(ctypes.Structure):
-    _fields_ = [
-        ("bmiHeader", BITMAPINFOHEADER)
-    ]
-
-
 user32 = ctypes.windll.user32
 gdi32 = ctypes.windll.gdi32
 
-w, h = 3440, 1440
 
-hwndDC = user32.GetDC(None)
-saveDC = gdi32.CreateCompatibleDC(hwndDC)
-# Get bitmap
-bmp = gdi32.CreateCompatibleBitmap(hwndDC, w, h)
-gdi32.SelectObject(saveDC, bmp)
+class BITMAP(ctypes.Structure):
+    _fields_ = [
+        ("bmType", ctypes.c_long),
+        ("bmWidth", ctypes.c_long),
+        ("bmHeight", ctypes.c_long),
+        ("bmWidthBytes", ctypes.c_long),
+        ("bmPlanes", wintypes.DWORD),
+        ("bmBitsPixel", wintypes.DWORD),
+        ("bmBits", ctypes.c_void_p)
+    ]
 
-# Init bitmap info
-# We grab the image in RGBX mode, so that each word is 32bit and
-# we have no striding, then we transform to RGB
-buffer_len = h * w * 4
-bmi = BITMAPINFO()
-bmi.bmiHeader.biSize = ctypes.sizeof(BITMAPINFOHEADER)
-bmi.bmiHeader.biWidth = w
-bmi.bmiHeader.biHeight = -h  # Why minus? See [1]
-bmi.bmiHeader.biPlanes = 1  # Always 1
-bmi.bmiHeader.biBitCount = 32
-bmi.bmiHeader.biCompression = 0
-# Blit
-image = ctypes.create_string_buffer(buffer_len)
-bits = gdi32.GetDIBits(saveDC, bmp, 0, h, image, bmi, 0)
-assert bits == h
-# Replace pixels values: BGRX to RGB
-image2 = ctypes.create_string_buffer(h * w * 3)
-image2[0::3] = image[2::4]
-image2[1::3] = image[1::4]
-image2[2::3] = image[0::4]
 
-img = bytes(image)
-print(type(img), len(img))
-for i in range(60):
-    print(img[i])
+t1 = time.perf_counter_ns()
+
+hdc = user32.GetDC(None)
+hbmp = win32gui.GetCurrentObject(hdc, win32con.OBJ_BITMAP)
+bmp = win32gui.GetObject(hbmp)
+print(bmp)
+
+# https://blog.csdn.net/alphabuilder/article/details/7555063
+# http://timgolden.me.uk/pywin32-docs/win32gui__GetCurrentObject_meth.html
+# http://timgolden.me.uk/pywin32-docs/win32gui__GetObject_meth.html
+# http://timgolden.me.uk/pywin32-docs/PyBITMAP.html
+
+user32.ReleaseDC(None, hdc)
+
+t2 = time.perf_counter_ns()
+
+print(f'耗时 {t2 - t1} ns')
